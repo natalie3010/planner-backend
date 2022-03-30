6976e7965b48e4e74ba8af37825b19857a1c3715
James <tom.james@capgemini.com>
2022-03-30 15:02:23 +0100

    Removed the access database. Added the actual sqlite .db file.@

--- sql_getter.js
+++ sql_getter.js
@@ -1,14 +1,26 @@
 const sqlite3 = require("sqlite3").verbose();
 
-let db = new sqlite3.Database("Skills_Supply_And_Demand.accdb", (err) => {
-    if(err) {
+// Open the database.
+let db = new sqlite3.Database("Workforce_Planning_02.sqbpro", (err) => {
+    if (err) {
         return console.error(err.message);
     }
     console.log("Connected to in-memory SQlite database.")
 });
 
-// Code that does something.
+// Query the skills list table.
+let sql_skills = "select * from Skills";
 
+db.all(sql_skills, [], (err, rows) => {
+    if (err) {
+        throw err;
+    }
+    rows.forEach((row) => {
+        console.log(row.name);
+    });
+});
+
+// Close the database.
 db.close((err) => {
     if (err) {
         return console.error(err.message);
