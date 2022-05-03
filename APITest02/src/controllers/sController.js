b76a8cdf8d89776417333434fdb61d5d9b76178c
Guegan <yannick.guegan@capgemini.com>
2022-05-03 13:31:58 +0100

    Fixed issues on dashboard sql request

--- APITest02/src/controllers/sController.js
+++ APITest02/src/controllers/sController.js
@@ -4,7 +4,7 @@ const db = new sqlite3("./Workforce_Planning_02.db", {fileMustExist: true});
 
 const sql_query = 
 
-`select * from 'Supply', 'Skills' WHERE Skills.SkillsID=Supply.SkillsID AND Skills.SkillName=?`;
+`select * from 'Supply', 'Skills', 'Demand' WHERE Skills.SkillsID=Demand.SkillsID AND Skills.SkillName=? AND Demand.SelectedApplicantID=Supply.ApplicantID`;
 
 export const getSupplyData = (req, res) => {
     let data = db.prepare(sql_query).all(req.selectedSkills);
--- APITest02/src/controllers/sdController.js
+++ APITest02/src/controllers/sdController.js
@@ -5,16 +5,16 @@ const db = new sqlite3("./Workforce_Planning_02.db", {fileMustExist: true});
 const sql_query = 
 
 `select demand_sq.'skill_name', demand_sq.'demand_count', supply_sq.'supply_count'
-from
-	(select Skills.SkillName as 'skill_name', count(Demand.SkillsID) as 'demand_count'
-	from Skills left join Demand 
-	on Skills.SkillsID = Demand.SkillsID
-	group by Skills.SkillName) demand_sq,
-	(select Skills.SkillName as 'skill_name', count(Supply.SkillsID) as 'supply_count'
-	from Skills left outer join Supply
-	on Skills.SkillsID = Supply.SkillsID
-	group by Skills.SkillName) supply_sq
-where demand_sq.'skill_name' = supply_sq.'skill_name'`;
+	from
+		(select Skills.SkillName as 'skill_name', count(Demand.SkillsID) as 'demand_count'
+		from Skills left join Demand 
+		on Skills.SkillsID = Demand.SkillsID
+		group by Skills.SkillName) demand_sq,
+		(select Skills.SkillName as 'skill_name', count(Demand.SkillsID) as 'supply_count'
+		from Skills left outer join Demand
+		on Skills.SkillsID = Demand.SkillsID and Demand.SelectedApplicantID not null
+		group by Skills.SkillName) supply_sq
+	where demand_sq.'skill_name' = supply_sq.'skill_name'`;
 
 export const getSupplyDemandData = (req, res) => {
     let data = db.prepare(sql_query).all();
