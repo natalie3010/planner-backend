f53d2ec4c2e34e09b6b22e4e87fe43547e7d4452
Guegan <yannick.guegan@capgemini.com>
2022-05-04 09:31:28 +0100

    added flag into logout response

--- APITest02/src/routes/logoutRoute.js
+++ APITest02/src/routes/logoutRoute.js
@@ -3,7 +3,9 @@ const logoutRoute = (app) => {
         .get((req, res) => {
             try {
                 req.session.destroy();
-                res.status(200);
+                res.status(200).json({
+                    authenticated:false
+                });
             } catch(err) {
                 res.status(500).json({ message: `Request failed with ${err}`});
             }
