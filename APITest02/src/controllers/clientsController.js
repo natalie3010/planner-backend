24e583d530afe82ca9ed18136ea0fc4002479f05
Guegan <yannick.guegan@capgemini.com>
2022-07-29 15:45:04 +0100

    added edit and update client

--- APITest02/src/controllers/clientsController.js
+++ APITest02/src/controllers/clientsController.js
@@ -2,13 +2,19 @@ import sqlite3 from 'better-sqlite3';
 
 const db = new sqlite3("./Workforce_Planning_02.db", {fileMustExist: false});
 
-const sql_query = 
-
-`select * from 'Clients'`;
+const SQL_QUERY_SELECT_ALL_CLIENTS = `select * from 'Clients'`;
 const SQL_QUERY_DELETE_CLIENT_BY_ID = `DELETE FROM 'Clients' WHERE ClientID=?`;
+const SQL_QUERY_INSERT_NEW_CLIENT = `INSERT INTO Clients(ClientID, ClientName) VALUES (?, ?)`;
+const SQL_QUERY_UPDATE_CLIENT_BY_ID = `
+UPDATE Clients 
+SET 
+    ClientName = ?
+WHERE
+    ClientID = ?
+`;
 
 export const getClientsData = () => {
-    let data = db.prepare(sql_query).all();
+    let data = db.prepare(SQL_QUERY_SELECT_ALL_CLIENTS).all();
     console.log(data);
 	return data;
 }
@@ -17,4 +23,17 @@ export const removeClientByID = (clientID) => {
     let confirmation = db.prepare(SQL_QUERY_DELETE_CLIENT_BY_ID).run(clientID);
     console.log(confirmation);
 	return confirmation;
+}
+
+export const addNewClient = (client) => {
+    console.log(client);
+    let data = db.prepare(SQL_QUERY_INSERT_NEW_CLIENT).run(client.ClientID, client.ClientName);
+    console.log(data);
+	return data;
+}
+
+export const updateExistingClient = (client, clientID) => {
+    let data = db.prepare(SQL_QUERY_UPDATE_CLIENT_BY_ID).run(client.ClientName, clientID);
+    console.log(data);
+	return data;
 }
--- APITest02/src/routes/clientsRoutes.js
+++ APITest02/src/routes/clientsRoutes.js
@@ -1,5 +1,5 @@
 
-import { getClientsData, removeClientByID} from '../controllers/clientsController';
+import { getClientsData, removeClientByID, addNewClient, updateExistingClient} from '../controllers/clientsController';
 import { getApplicationAccessControlDefinition } from '../helpers/accessControlHelper';
 import { Router } from 'express';
 import { AccessControl } from 'accesscontrol';
@@ -34,6 +34,36 @@ clientsRoutes.delete('/:clientID', (req, res) => {
     }
     else {
         res.status(403).json('User not authorised to proceed');
+    }
+});
+
+clientsRoutes.post('/', (req, res) => {
+    const accessControl = new AccessControl(getApplicationAccessControlDefinition())
+    if(accessControl.can(req.decodedToken.role).updateAny('demand').granted) {
+        try {
+            const data = addNewClient(req.body);
+            res.status(200).json(data);
+        } catch(err) {
+            res.status(500).json({ message: `Request failed with ${err}`});
+        }
+    }
+    else {
+        res.status(403).json('User not authorised to proceed');
+    }
+});
+
+clientsRoutes.put('/:clientID', (req, res) => {
+    const accessControl = new AccessControl(getApplicationAccessControlDefinition())
+    if(accessControl.can(req.decodedToken.role).updateAny('demand').granted) {
+        try {
+            const data = updateExistingClient(req.body, req.params.clientID);
+            res.status(200).json(data);
+        } catch(err) {
+            res.status(500).json({ message: `Request failed with ${err}`});
+        }
+    }
+    else {
+        res.status(403).send('User not authenticated');
     }
 });
 
