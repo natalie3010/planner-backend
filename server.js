70992e95bdce1ee5b9bec7fd8e3a777207fa2a0e
peterjanik <peter.janik@capgemini.com>
2022-10-21 09:51:20 +0100

    remove http

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import dotenv from 'dotenv'
 import { app } from './app'
-import { Http, Https } from './httpHttps'
+import { Https } from './httpHttps'
 
 dotenv.config()
 
 try {
-  Http(app)
   Https(app)
 } catch (error) {
   console.log('Server boot error', { error })
