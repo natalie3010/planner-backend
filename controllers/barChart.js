7b72a0864be815032136cdb3def4bfeb4a8f42f2
peterjanik <peter.janik@capgemini.com>
2022-10-24 12:39:34 +0100

    Remove console log

--- controllers/barChart.js
+++ controllers/barChart.js
@@ -5,8 +5,7 @@ export const barchartController = {
     try {
       res.status(200).json(barChartData)
     } catch (err) {
-      console.log(err)
-      // res.status(500).json({ message: err.message })
+      res.status(500).json({ message: err.message })
     }
   },
 }
