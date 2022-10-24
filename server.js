06cceacf2392353caf07fa91dcd86c1fff877c55
peterjanik <peter.janik@capgemini.com>
2022-10-24 11:57:16 +0100

    add http in dev

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
 import { app } from './app'
-import { Https } from './httpHttps'
+import { Https, Http } from './httpHttps'
 
 dotenv.config()
 
 try {
+  process.env.NODE_ENV === 'development' && Http(app)
   Https(app)
 } catch (error) {
   console.log('Server boot error', { error })
