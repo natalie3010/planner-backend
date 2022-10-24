f66c5953e1e3fe72d815873c2e77bfdfc73a5b29
peterjanik <peter.janik@capgemini.com>
2022-10-24 17:12:52 +0100

    remove https

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv'
 import { app } from './app'
-import { Https, Http } from './httpHttps'
 
 dotenv.config()
 
+const PORT = process.env.PORT || 80
+
 try {
-  process.env.NODE_ENV === 'development' && Http(app)
-  Https(app)
+  app.listen(PORT, () => {
+    console.log(`Http server running at port ${PORT}/`)
+  })
 } catch (error) {
   console.log('Server boot error', { error })
 }
